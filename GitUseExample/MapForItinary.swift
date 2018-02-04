a432b120c0d837c060b4fa18baa55c23bf2e4967
Marin Bricq <bricqmarin@yahoo.fr>
2018-02-04 11:38:09 +0100

    Add MapForItinary class

---
+++ GitUseExample/MapForItinary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinary.swift
+//  GitUseExample
+//
+//  Created by Marin on 04/02/2018.
+//  Copyright © 2018 Marin. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinary: NSObject {
+
+}
